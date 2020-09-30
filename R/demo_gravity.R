fa01a97f4a7f238d1d5f7e9681ffeecfdd301bf5
Greg Macfarlane <gregmacfarlane@gmail.com>
2020-09-30 08:29:35 -0600

    Gravity model

---
+++ R/demo_gravity.R
@@ -0,0 +1,48 @@
+# inputs
+p <- c(100, 200, 100)
+a <- c(200, 50, 150)
+C <- matrix(c(2,5,4,5,2,3,4,3,2), nrow=3, byrow=TRUE)
+
+trips <- matrix(0, nrow = 3, ncol = 3)
+b <- 1.5
+
+for (i in 1:3) {
+  
+  bottomA <- sum(a * C[i, ]^(-b))
+  
+  for (j in 1:3) {
+    topA <- a[j] * C[i,j]^(-b)
+    trips[i, j] <- p[i] * topA / bottomA
+  }
+}
+
+rowSums(trips)
+colSums(trips)
+
+#' Gravity Model Function
+#' @param p vector of productions, length n
+#' @param A vector of attractions, lenth n
+#' @param C matrix of impedances, dim n x n
+#' @param b impedance parameter
+gravity <- function(p, a, C, b){
+  # output matrix (all 0 here)
+  trips <- matrix(0, nrow = length(p), ncol = length(a)) 
+  # loop over all rows (production)
+  for (i in 1:length(p)) {
+    bottomA <- sum(a * C[i, ]^(-b)) # denominator
+    
+    # loop over all columns (attraction)
+    for (j in 1:length(a)) {
+      # calculate gravity model for trips from i to j
+      topA <- a[j] * C[i,j]^(-b)
+      trips[i, j] <-  p[i] * topA / bottomA
+    }
+  }
+  
+  return(trips)
+}
+
+
+
+gravity(p, a, C, 1.5)
+gravity(p, a, C, 2)
