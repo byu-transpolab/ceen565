16b7f51c0aa68edc51084b6f7aba0bf78e43dedc
Greg Macfarlane <gregmacfarlane@byu.edu>
2020-10-20 11:13:41 -0600

    Add NL / IIA notes

--- R/demo_mnl.R
+++ R/demo_mnl.R
@@ -23,6 +23,40 @@ rho2C <- 1 - llB / llC
 
 # estimate model with total travel time and workplace employment density
 mnl2 <- mlogit(CHOSEN ~ TVTT | WKEMPDEN, data = worktrips)
+mnl3 <- mlogit(CHOSEN ~ OVTT + IVTT, data = worktrips)
 
 # compare the models against each other.
 modelsummary(list(mnl1, mnl2), statistic = "std.error", stars = TRUE, statistic_vertical = FALSE)
+
+nl <- mlogit(CHOSEN ~ TVTT | WKEMPDEN, data = worktrips,
+             nests = list(
+               auto = c('Drive Alone', 'Share 2', 'Share 3+'),
+               nonauto = c('Bike', 'Walk', 'Transit')
+             ),
+             un.nest.el = TRUE)
+
+
+worktrips_li <- worktrips %>% filter(HHINC <= 50)
+worktrips_hi <- worktrips %>% filter(HHINC > 50)
+
+nl_li <- mlogit(CHOSEN ~ TVTT | WKEMPDEN, data = worktrips_li,
+             nests = list(
+               auto = c('Drive Alone', 'Share 2', 'Share 3+'),
+               nonauto = c('Bike', 'Walk', 'Transit')
+             ),
+             un.nest.el = TRUE)
+
+nl_hi <- mlogit(CHOSEN ~ TVTT | WKEMPDEN, data = worktrips_hi,
+             nests = list(
+               auto = c('Drive Alone', 'Share 2', 'Share 3+'),
+               nonauto = c('Bike', 'Walk', 'Transit')
+             ),
+             un.nest.el = TRUE)
+
+worktrips_p1 <- worktrips %>% filter(idx$id %in% c("3_1", "2_1"))
+
+a <- predict(mnl1, worktrips, type = "linpred")[1:5, ]
+b <- predict(mnl1, worktrips)[1:5, ]
+
+texreg::screenreg(list(nl_li, nl_hi))
+
